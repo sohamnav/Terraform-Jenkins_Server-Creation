003537c3eccdba93730a327ac2e350867aac51bd
Soham Navale <102356083+sohamnav@users.noreply.github.com>
2025-12-25 22:27:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app"
-    region = "us-east-1"
-    key = "eks/terraform.tfstate"
+    bucket = "project-1-infrastructure-based-terraform-state"
+    region = "ap-south-1"
+    key = "project-1/terraform.tfstate"
   }
 }
